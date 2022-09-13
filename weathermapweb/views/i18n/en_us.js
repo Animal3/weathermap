6d26904a27224b67c394bf82cfc5efb4a00d5087
DevCloud_Luis <ivu@live.com.mx>
2022-09-13 13:51:47 -0500

    Update en_us.js

--- weathermapweb/views/i18n/en_us.js
+++ weathermapweb/views/i18n/en_us.js
@@ -1,6 +1,6 @@
 var globalLanguageEn = {
 	"weatherDemo": "Weather forecast CSE-Demo-2.0",
-	"cityWeather": "City Weather DEMO for Coppel",
+	"cityWeather": "City Weather",
 	"search": "Search",
 	"weatherDetails": "Current Weather Details And Forecast For The Next 5 Days",
 	"yourCity": "Your city name (all fight)",
